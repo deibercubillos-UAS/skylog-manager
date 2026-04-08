1d7e76683a5cc0de4332811f3031e060ad37db67
deibercubillos-UAS <deibercubillos@gmail.com>
2026-04-08 04:36:08 +0000

    STABLE: Rollback to 73f7888 and downgrade to Next.js 14.2.23 LTS for production stability

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configuración limpia para evitar errores de Build
   images: {
     unoptimized: true,
   }
 };
-
 export default nextConfig;

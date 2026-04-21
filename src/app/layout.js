5cdcca6fea0681419f31c67d97f918b19e515be9
deibercubillos-UAS <deibercubillos@gmail.com>
2026-04-21 03:19:20 +0000

    MASTER: Post-audit performance optimization complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-  <script dangerouslySetInnerHTML={{
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700;800;900&family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=swap" rel="stylesheet" />
+        <script dangerouslySetInnerHTML={{
     __html: `
       (function() {
         var originalAddEventListener = EventTarget.prototype.addEventListener;
